c65d3fa9b86c529373fb663b413fecf291e79b2c
endiesworld <endiesworld@gmail.com>
2024-01-15 04:15:12 +0100

    feat: Added users/username/inventory route

--- inventory_frontend/app/routes/signin.tsx
+++ inventory_frontend/app/routes/signin.tsx
@@ -1,9 +1,18 @@
+import {redirect, type ActionFunctionArgs, } from '@remix-run/node'
 import {Form } from '@remix-run/react'
 import { Button } from '../components/ui/reusable/button'
 import { Input } from '../components/ui/reusable/input'
 import InventorySVG from '../assets/ilustration2.svg'; // Replace with the actual path to your SVG file
 
+export async function action({
+    request,
+}: ActionFunctionArgs) {
 
+    const body = await request.formData();
+    const username = body.get("username")
+    const redirect_ = `/users/${username}/inventory`
+    return redirect(redirect_);
+}
 
 export default function Signin() {
 
@@ -14,7 +23,7 @@ export default function Signin() {
                 alt="Inventory Service"
                 className="w-1/2 h-screen object-center"
             />
-            <Form action="/" method="post" className="bg-blue-500 p-8 shadow-md rounded-md italic font-bold flex h-1/3 w-2/3 flex-col gap-y-4 overflow-x-hidden px-10 pb-28 pt-12 max-w-xl">
+            <Form method="POST" className="bg-blue-500 p-8 shadow-md rounded-md italic font-bold flex h-1/3 w-2/3 flex-col gap-y-4 overflow-x-hidden px-10 pb-28 pt-12 max-w-xl">
                 <label htmlFor='username' > Username</label>
                 <Input name="username" type="text" defaultValue='username'/>
                 <label htmlFor='password'> Password</label>
---
+++ inventory_frontend/app/routes/users.$username.inventory.tsx
@@ -0,0 +1,34 @@
+import  { json } from "@remix-run/node";
+import Wrapper from '../components/ui/wrapper';
+import Products from '../components/ui/products';
+import { useLoaderData } from "@remix-run/react";
+
+// Type to describe the data we get from the API
+type Inventory = {
+    name: string;
+    price: number;
+    quantity: number;
+    id: string
+};
+
+export async function loader(){
+    const response = await fetch("http://localhost:5000/api/inventory/products");
+
+    const products: Inventory[] = await response.json();
+    
+    // Return the data as JSON
+    return json({ products });
+
+}
+
+export default function Inventory() {
+    const {products} = useLoaderData<typeof loader>();
+    
+    return (
+        <div className="w-screen h-screen">
+            <Wrapper>
+                <Products products_={products}/>
+            </Wrapper>
+        </div>
+    );
+}
